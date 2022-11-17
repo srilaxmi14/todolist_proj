f0cef1700a536a3e2e8f9934e239a77ecadb76fd
Shetty <srilaxmi.shetty@wipfli.com>
2022-11-17 11:08:17 +0530

    update

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 window.addEventListener('load', () => {
-	todos = JSON.parse(localStorage.getItem('todos'));
+	todos = JSON.parse(localStorage.getItem('todos')); //convert string to object
+ console.log(todos);
+	const newTodoForm = document.querySelector('#new-todo-form');//take value from form
+	const completedtask=document.getElementById("completed task");
 
-	const newTodoForm = document.querySelector('#new-todo-form');
-
-	newTodoForm.addEventListener('submit', e => {
-		e.preventDefault();
-        
+	newTodoForm.addEventListener('submit', e => { //it works only when we submit add task
+	    e.preventDefault();
+        console.log(e);
 		const todo = {
-			content: e.target.elements.content.value,
+			content: e.target.content.value
 		}
 
-		todos.push(todo);
+	    todos.unshift(todo);//push object to todos
+		console.log(todos);
 
-		localStorage.setItem('todos', JSON.stringify(todos));
-
-		// Reset the form
+		localStorage.setItem('todos', JSON.stringify(todos));//after refresh the data should not be lost //todos is an array of objects 
+	// 	// Reset the form
 		e.target.reset();
 
-		DisplayTodos()
+		DisplayTodos();
 	})
 
-	DisplayTodos()
+	DisplayTodos();
 })
 
 function DisplayTodos () {
 	const todoList = document.querySelector('#todo-list');
+	console.log(todoList);
+
 	todoList.innerHTML = "";
 
 	todos.forEach(todo => {
+		console.log(todo);
+		console.log(todos);
 		const todoItem = document.createElement('div');
 		todoItem.classList.add('todo-item');
 
@@ -36,67 +41,50 @@ function DisplayTodos () {
 		const span = document.createElement('span');
 		const content = document.createElement('div');
 		const actions = document.createElement('div');
-		const edit = document.createElement('button');
 		const deleteButton = document.createElement('button');
 
 		input.type = 'checkbox';
-		input.checked = todo.done;
-		span.classList.add('bubble');
+		input.checked = todo.done; //done is boolean value to check 
+		//span.classList.add('bubble'); //
 		content.classList.add('todo-content');
 		actions.classList.add('actions');
-		edit.classList.add('edit');
 		deleteButton.classList.add('delete');
 
 		content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
-		edit.innerHTML = 'Edit';
 		deleteButton.innerHTML = 'Delete';
 
 		label.appendChild(input);
 		label.appendChild(span);
-		actions.appendChild(edit);
 		actions.appendChild(deleteButton);
 		todoItem.appendChild(label);
 		todoItem.appendChild(content);
 		todoItem.appendChild(actions);
-
 		todoList.appendChild(todoItem);
 
 		if (todo.done) {
-			todoItem.classList.add('done');
+			todoItem.classList.add('done');//line throught if completed
 		}
+
 		
-		input.addEventListener('change', (e) => {
+		input.addEventListener('change', (e) => { //if we want to change when edit button is clicked
 			todo.done = e.target.checked;
 			localStorage.setItem('todos', JSON.stringify(todos));
-
 			if (todo.done) {
 				todoItem.classList.add('done');
+
 			} else {
 				todoItem.classList.remove('done');
 			}
-
 			DisplayTodos()
 
 		})
 
-		edit.addEventListener('click', (e) => {
-			const input = content.querySelector('input');
-			input.removeAttribute('readonly');
-			input.focus();
-			input.addEventListener('blur', (e) => {
-				input.setAttribute('readonly', true);
-				todo.content = e.target.value;
-				localStorage.setItem('todos', JSON.stringify(todos));
-				DisplayTodos()
-
-			})
-		})
-
 		deleteButton.addEventListener('click', (e) => {
-			todos = todos.filter(t => t != todo);
+			todos = todos.filter(t => t != todo); 
 			localStorage.setItem('todos', JSON.stringify(todos));
 			DisplayTodos()
 		})
 
 	})
 }
+
